a3da457a5d79535f777c73592a2f09a4dba69ba3
Nahuel <na.manterola@gmail.com>
2020-05-14 15:20:22 +0200

    Implemented pair improvement

--- src/dlibFaceSelect.cpp
+++ src/dlibFaceSelect.cpp
@@ -4,10 +4,40 @@
 #include <dlib/image_processing.h>
 #include <dlib/image_io.h>
 #include <dlib/opencv.h>
+#include "functions.h"
 
 #include <opencv2/imgproc.hpp>
 
-cv::Mat getFaceMask(cv::Mat& image)
+cv::Mat getFaceMask()
+{
+    std::string folder = "Images";
+    std::vector<std::string> files = getImagesPathsFromFolder(folder);
+    std::vector<cv::Mat> images;
+    cv::Mat image = cv::imread(files[12], cv::IMREAD_GRAYSCALE);
+    resize(image, image, cv::Size(), 0.25, 0.25);
+
+    dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
+    dlib::shape_predictor sp;
+    dlib::deserialize("shape_predictor_68_face_landmarks.dat") >> sp;
+    IplImage iplImage = cvIplImage(image);
+    dlib::cv_image<uchar> img(iplImage);
+    std::vector<dlib::rectangle> faces = detector(img);
+    dlib::full_object_detection shape = sp(img, faces[0]);
+
+    cv::Mat mask{ image.size(), CV_8U, cv::Scalar(0) };
+
+    std::vector<cv::Point2i> PointIndices;
+    for (int i = 0; i < 17; i++)
+        PointIndices.push_back(cv::Point2i(shape.part(i).x(), shape.part(i).y()));
+    PointIndices.push_back(cv::Point2i(shape.part(24).x(), shape.part(24).y()));
+    PointIndices.push_back(cv::Point2i(shape.part(19).x(), shape.part(19).y()));
+
+    fillConvexPoly(mask, PointIndices, cv::Scalar(255), 16, 0);
+    return mask;
+}
+
+
+cv::Mat getFaceCircle(cv::Mat& image)
 {
     dlib::frontal_face_detector detector = dlib::get_frontal_face_detector();
     dlib::shape_predictor sp;
--- src/functions.cpp
+++ src/functions.cpp
@@ -4,10 +4,113 @@
 
 #include "functions.h"
 #include "Camera.h"
+#include "dlibFaceSelect.h"
 
 using namespace cv;
 
-std::vector<std::array<int, 2>> getCameraPairs(std::vector<Camera>& cameras, pairType pair) {
+
+cv::Mat shiftPerspective(Camera inputCam, Camera outputCam, cv::Mat depthMap, cv::Mat mask)
+{
+    Mat shiftedDepthMap = Mat{ depthMap.size() , depthMap.type() };
+    double preMultX = (inputCam.pos3D.x - outputCam.pos3D.x) * inputCam.f / inputCam.pixel_size;
+    double preMultY = (inputCam.pos3D.y - outputCam.pos3D.y) * inputCam.f / inputCam.pixel_size;
+    Point2i halfRes = depthMap.size() / 2;
+    for (int x = 0; x < shiftedDepthMap.cols; x++) {
+        for (int y = 0; y < shiftedDepthMap.rows; y++) {
+            
+            Point3d vec = outputCam.inv_project(Point2i{ x, y }-halfRes);
+            Point3d p1 = outputCam.pos3D + (vec * 0.5);
+            Point3d p2 = outputCam.pos3D + vec;
+            Point2i pixel1 = inputCam.project(p1) + halfRes;
+            Point2i pixel2 = inputCam.project(p2) + halfRes;
+
+            std::vector<Point2i> pixels = bresenham(pixel1, pixel2);
+            std::vector<float> error;
+
+            for (auto p : pixels) {
+                Rect selector = Rect{ p - Point(kernelSize, kernelSize), p + Point(kernelSize, kernelSize) };
+                Mat selection = images[pair[1]](selector);
+                Mat result{ CV_32FC1 };
+                error.push_back(getAbsDiff(selection, kernel));
+            }
+
+            int maxIndex = std::distance(error.begin(), std::min_element(error.begin(), error.end()));
+
+            Point2i pixel = pixels[maxIndex];
+            depth.at<double>(Point(x, y)) = depth.at<double>(Point(x, y)) + preMult / (norm(pixel - Point2i{ x, y }));
+
+        }
+    }
+}
+
+cv::Mat shiftPerspective2(Camera inputCam, Camera outputCam, cv::Mat depthMap)
+{
+    Mat shiftedDepthMap = Mat{ depthMap.size() , depthMap.type() };
+    double preMultX = (inputCam.pos3D.x - outputCam.pos3D.x) * inputCam.f / inputCam.pixel_size;
+    double preMultY = (inputCam.pos3D.y - outputCam.pos3D.y) * inputCam.f / inputCam.pixel_size;
+    int direction[] = { outputCam.pos3D.x - inputCam.pos3D.x, outputCam.pos3D.y - inputCam.pos3D.y };
+    //std::cout << "direction0 " << direction[0] << std::endl;
+    for (int x = 0; x < depthMap.cols; x++) {
+        for (int y = 0; y < depthMap.rows; y++) {
+            double depth = depthMap.at<double>(y,x);
+            if (depth < 0.5) 
+                continue;
+            int shiftedX = int(preMultX / depth) + x;
+            int shiftedY = int(preMultY / depth) + y;
+            if (shiftedY >= depthMap.rows || shiftedY < 0 || shiftedX >= depthMap.cols || shiftedX < 0) 
+                continue;
+            shiftedDepthMap.at<double>(Point(shiftedX, shiftedY)) = depth;
+        }
+    }
+
+    //cv::imshow("Map", shiftedDepthMap);
+    //cv::imshow("Original", depthMap);
+    //cv::waitKey(0);
+    return shiftedDepthMap;
+}
+
+std::vector< std::vector<std::array<int, 2>> > getGroups(std::vector<Camera> &cameras, std::string groupType)
+{
+    std::vector< std::vector<std::array<int, 2>> > groups;
+    if (groupType == "CHESS") {
+        for (int i = 0; i < 25; i += 2) {
+            groups.push_back(getCameraPairs(cameras, CROSS, i));
+        }
+    }
+    return groups;
+}
+
+cv::Mat Points3DToDepthMap(std::vector<Point3d>& points, Camera camera, cv::Size resolution)
+{
+    Mat depthMap = Mat{ resolution, CV_64FC1 };
+    std::cout << depthMap.size() << std::endl;
+    Point2i halfRes = resolution / 2;
+    for (auto p : points) 
+    {
+        Point2i pixel = camera.project(p) + halfRes;
+        if (pixel.x >= 0 && pixel.x < resolution.width && pixel.y >= 0 && pixel.y < resolution.height) {
+            //std::cout << p << ", " << pixel << std::endl;
+            depthMap.at<double>(pixel) = p.z - camera.pos3D.z;
+        }
+    }
+    return depthMap;
+}
+
+std::vector<Point3d> DepthMapToPoints3D(cv::Mat& depthMap, Camera camera, cv::Size resolution)
+{
+    Point2i halfRes = resolution / 2;
+    std::vector<Point3d> Points;
+    for (int u = 0; u < depthMap.cols; u++) {
+        for (int v = 0; v < depthMap.rows; v++) {
+            double depth = depthMap.at<double>(Point(u, v));
+            if (depth > 0.1)
+                Points.push_back( camera.pos3D + camera.inv_project(Point(u,v)-halfRes) * depth );
+        }
+    }
+    return Points;
+}
+
+std::vector<std::array<int, 2>> getCameraPairs(const std::vector<Camera>& cameras, const pairType pair) {
     std::vector<std::array<int, 2>> pairs;
     if (pair == TO_CENTER) {
         for (int i = 0; i < cameras.size(); i++) {
@@ -28,6 +131,9 @@ std::vector<std::array<int, 2>> getCameraPairs(std::vector<Camera>& cameras, pai
     else if (pair == MID_LEFT) {
         pairs.push_back({ 12, 11 });
     }
+    else if (pair == MID_TOP) {
+        pairs.push_back({ 12, 7 });
+    }
     else if (pair == LINE_HORIZONTAL) {
         for (int i = 10; i < 15; i++) {
             if (i == 12) continue;
@@ -40,17 +146,33 @@ std::vector<std::array<int, 2>> getCameraPairs(std::vector<Camera>& cameras, pai
             pairs.push_back({ 12, i });
         }
     }
-    else if (pair == LINE_VERTICAL) {
-        for (int i = 2; i < 25; i += 5) {
-            if (i == 12) continue;
-            pairs.push_back({ 12, i });
-        }
-    }
     else if (pair == CROSS) {
             pairs.push_back({ 12, 11 });
             pairs.push_back({ 12, 13 });
             pairs.push_back({ 12, 7 });
             pairs.push_back({ 12, 17 });
+    }
+    else if (pair == JUMP_CROSS) {
+        pairs.push_back({ 12, 10 });
+        pairs.push_back({ 12, 14 });
+        pairs.push_back({ 12, 2 });
+        pairs.push_back({ 12, 24 });
+    }
+    return pairs;
+}
+
+std::vector<std::array<int, 2>> getCameraPairs(const std::vector<Camera>& cameras, const pairType pair, const int cameraNum) {
+    std::vector<std::array<int, 2>> pairs;
+    if (pair == CROSS) {
+        if(cameraNum-5>0)
+            pairs.push_back({ cameraNum, cameraNum - 5 });
+        if(cameraNum+5<25)
+            pairs.push_back({ cameraNum, + 5 });
+        if(cameraNum%5>0)
+            pairs.push_back({ cameraNum, cameraNum - 1 });
+        if (cameraNum % 5 < 4)
+            pairs.push_back({ cameraNum, cameraNum + 1 });
+        
     }
     return pairs;
 }
@@ -60,10 +182,22 @@ double getAbsDiff(cv::Mat& mat1, cv::Mat& mat2)
     return sum(abs(mat1-mat2))[0];
 }
 
-void showImage(std::string name, Mat image) {
+void CallBackFuncs(int event, int x, int y, int flags, void* param)
+{
+    Mat* ptrImage = (Mat*)param;
+    if (event == EVENT_LBUTTONDOWN)
+    {
+        std::cout << "at position (" << x << ", " << y << ")" << ": " << ptrImage->at<unsigned char>(Point(x, y)) << std::endl;
+    }
+
+
+}
+
+void showImage(std::string name, Mat &image) {
     namedWindow(name, WINDOW_NORMAL);
     resizeWindow(name, 710, 540);
     imshow(name, image);
+    setMouseCallback(name, CallBackFuncs, (void*)&image);
 }
 
 std::vector<std::string> getImagesPathsFromFolder(std::string folderPath)
@@ -146,4 +280,37 @@ std::vector<Point2i> bresenham(Point2i point2, Point2i point1)
             return plotLineHigh(point1.x, point1.y, point2.x, point2.y);
         }
     }
+}
+
+cv::Mat getIdealRef() {
+    Mat R;
+    cv::FileStorage file;
+    file.open("idealRef.yml", cv::FileStorage::READ);
+    file["R"] >> R;
+    return R;
+}
+
+void saveImage(std::string filename, cv::Mat image)
+{
+    cv::FileStorage file(filename, cv::FileStorage::WRITE);
+    // Write to file!
+    file << "image" << image;
+
+}
+
+cv::Mat loadImage(std::string filename)
+{
+    Mat R;
+    cv::FileStorage file;
+    file.open(filename, cv::FileStorage::READ);
+    file["image"] >> R;
+    return R;
+}
+
+double calculateAverageError(cv::Mat &image)
+{
+    std::string folder = "Images";
+    std::vector<std::string> files = getImagesPathsFromFolder(folder);
+    Mat mask = getFaceMask();
+    return cv::mean(image, mask)[0];
 }
